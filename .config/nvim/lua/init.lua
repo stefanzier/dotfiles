e2b2d215a19017453a282e68962ef1791eb30196
Stefan Zier <szier@icloud.com>
2022-05-30 17:16:24 -0700

    update nvim-tree setup

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -57,9 +57,19 @@ g['blamer_enabled'] = 1
 require("focus").setup()
 
 -- kyazdani42/nvim-tree.lua
-g.nvim_tree_show_icons = { git = 0, folders = 0, files = 0, folder_arrows = 0 }
+require'nvim-tree'.setup {
+  renderer = {
+    icons = {
+      show = {
+        file = false,
+        folder = false,
+        folder_arrow = false,
+        git = false,
+      },
+    },
+  }
+}
 map('n', '<leader>n', '<cmd>NvimTreeFindFileToggle<cr>')
-require'nvim-tree'.setup {}
 
 -- nvim-compe
 require'compe'.setup {
